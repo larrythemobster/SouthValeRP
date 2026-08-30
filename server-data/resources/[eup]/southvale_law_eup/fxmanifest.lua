43f274292274007d98e04e251fa94ee029d3c71f
adum12 <317056908+A-Adum@users.noreply.github.com>
2026-08-30 02:39:07 -0400

    Add law enforcement EUP uniforms

---
+++ server-data/resources/[eup]/southvale_law_eup/fxmanifest.lua
@@ -0,0 +1,10 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'SouthVale RP'
+description 'SouthVale law-enforcement EUP assets'
+version '1.0.0'
+
+files { 'stream/*.ydd', 'stream/*.ytd' }
+
