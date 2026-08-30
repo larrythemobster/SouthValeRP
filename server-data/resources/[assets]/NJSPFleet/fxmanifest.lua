8db6c98bede4149a122c78277e05a60302a61f3a
adum12 <317056908+A-Adum@users.noreply.github.com>
2026-08-30 03:11:25 -0400

    Replace police garage fleet

---
+++ server-data/resources/[assets]/NJSPFleet/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version 'adamant'
+game 'gta5'
+lua54 'yes'
+
+files {
+	'data/**/vehiclelayouts.meta',
+	'data/**/vehicles.meta',
+	'data/**/carcols.meta',
+	'data/**/dlctext.meta',
+	'data/**/carvariations.meta',
+	'data/**/carvariations_lc.meta',
+	'data/**/handling.meta',
+	'data/**/vehicle_names.meta',
+}
+
+data_file 'VEHICLE_LAYOUTS_FILE' 'data/**/vehiclelayouts.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/**/vehicles.meta'
+data_file 'VEHICLE_NAMES_FILE' 'data/**/vehicle_names.meta'
+data_file 'CARCOLS_FILE' 'data/**/carcols.meta'
+data_file 'DLCTEXT_FILE' 'data/**/dlctext.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/**/carvariations_lc.meta'
+data_file 'HANDLING_FILE' 'data/**/handling.meta'
+
